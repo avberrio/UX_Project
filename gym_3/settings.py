225104d22505f0116eafd9653c3d40f2adb66650
avberrio <aldo.berrios.13@sansano.usm.cl>
2017-12-19 18:27:35 -0300

    Communication between url, controllers and views was implemented

--- gym_3/settings.py
+++ gym_3/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'gym_3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'gym_app', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- gym_app/views.py
+++ gym_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def base(request):
+    context = {'latest_question_list': [1, 2, 3, 4]}
+    return render(request, 'base.html', context)
