096a9874e1fe7f5c8fee7c79fe06329ad3708c8a
avberrio <aldo.berrios.13@sansano.usm.cl>
2017-12-27 16:30:21 -0300

    Virtualenv added

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aldo/Estudios/2017-2/INF322 - Diseño de Interfaces Usuarias/gym_3/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
